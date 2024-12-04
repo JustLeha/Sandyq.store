1e3871cb10819df263364bd56062c5eca14b5937
evn <evenrichman@gmail.com>
2024-12-04 15:41:41 +0500

    qe

--- lib/_auth_screen.dart
+++ lib/_auth_screen.dart
@@ -1,16 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter/services.dart';  // Для обработки ошибок
 import '_main_authorization_and_registration.dart';  // Ваш экран регистрации
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
+  bool isLoading = false;
 
   Future<void> _signInWithGoogle(BuildContext context) async {
+    setState(() {
+      isLoading = true;
+    });
+
     try {
-      // Получение учетных данных через Google
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
       final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
 
@@ -21,25 +29,33 @@ class AuthScreen extends StatelessWidget {
 
       // Вход с использованием Google аккаунта
       await _auth.signInWithCredential(credential);
-      Navigator.push(
+      Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => RegisterScreen()),  // Переход на экран регистрации или главный экран
+        MaterialPageRoute(builder: (context) => PhoneAuthScreen()),  // Переход на экран регистрации или главный экран
       );
     } catch (e) {
       print('Error signing in with Google: $e');
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to sign in with Google')));
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 
   Future<void> _signInWithPhone(BuildContext context) async {
+    setState(() {
+      isLoading = true;
+    });
+
     try {
       await _auth.verifyPhoneNumber(
         phoneNumber: '+1 555-555-5555',  // Введите номер телефона для теста
         verificationCompleted: (PhoneAuthCredential credential) async {
           await _auth.signInWithCredential(credential);
-          Navigator.push(
+          Navigator.pushReplacement(
             context,
-            MaterialPageRoute(builder: (context) => RegisterScreen()),  // Переход на экран регистрации или главный экран
+            MaterialPageRoute(builder: (context) => PhoneAuthScreen()),  // Переход на экран регистрации или главный экран
           );
         },
         verificationFailed: (FirebaseAuthException e) {
@@ -47,17 +63,18 @@ class AuthScreen extends StatelessWidget {
           ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to verify phone number')));
         },
         codeSent: (String verificationId, int? resendToken) {
-          // Запрос кода подтверждения через SMS
           print('Code sent to phone number');
           ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Code sent to phone number')));
         },
-        codeAutoRetrievalTimeout: (String verificationId) {
-          // Тайм-аут для автоматического извлечения кода
-        },
+        codeAutoRetrievalTimeout: (String verificationId) {},
       );
     } catch (e) {
       print('Error signing in with phone: $e');
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to sign in with phone')));
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 
@@ -65,23 +82,47 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Authentication'),
+        title: Text('Authentication', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.blueAccent,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () => _signInWithGoogle(context),
-              child: Text('Sign in with Google'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _signInWithPhone(context),
-              child: Text('Sign in with Phone Number'),
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (isLoading)
+                CircularProgressIndicator()
+              else ...[
+                // Кнопка входа через Google
+                ElevatedButton.icon(
+                  onPressed: () => _signInWithGoogle(context),
+                  icon: Icon(Icons.g_mobiledata, size: 30),
+                  label: Text('Sign in with Google', style: TextStyle(fontSize: 16)),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black, backgroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                    side: BorderSide(color: Colors.grey),
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                // Кнопка входа через телефон
+                ElevatedButton.icon(
+                  onPressed: () => _signInWithPhone(context),
+                  icon: Icon(Icons.phone, size: 30),
+                  label: Text('Sign in with Phone Number', style: TextStyle(fontSize: 16)),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black, backgroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                    side: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ],
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import '_auth_screen.dart';
+import '_auth_screen.dart'; // Ваш экран авторизации
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized(); // Инициализация фреймворка
   await Firebase.initializeApp(); // Инициализация Firebase
   runApp(MyApp());
 }
@@ -12,8 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Authentication',
-      home: AuthScreen(),
+      title: 'Firebase App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AuthScreen(), // Экран авторизации
     );
   }
 }
